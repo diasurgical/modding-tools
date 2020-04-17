659b0841423abfd868173388700b4fec5a8e6275
Anders Jenbo <anders@jenbo.dk>
2020-04-17 02:16:02 +0200

    Set up basic tilsets

---
+++ Tiled/tilesets/monsters.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="monsters" tilewidth="160" tileheight="160" tilecount="132" columns="12">
+ <image source="monsters.png" width="1920" height="1760"/>
+</tileset>
---
+++ Tiled/tilesets/objects.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="objects" tilewidth="128" tileheight="128" tilecount="91" columns="13">
+ <image source="objects.png" width="1664" height="896"/>
+</tileset>
