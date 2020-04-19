61d27af7179ed2ab2f113a95cd1f5919dae1f666
Anders Jenbo <anders@jenbo.dk>
2020-04-19 20:06:21 +0200

    Deduplicate tilesets

--- Tiled/tilesets/tristram_mega_special.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.3" name="tristram" tilewidth="128" tileheight="288" tilecount="207" columns="32">
- <image source="tristram_mega_special.png" width="4096" height="3168"/>
-</tileset>
