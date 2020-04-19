7d1d7430a92e871edd712552a0c479663ff677d3
Robin Eklind <rnd0x00@gmail.com>
2020-04-19 17:38:41 +0200

    tilesets: add tilesets with special graphics (trees, arches) rendered on top

---
+++ Tiled/tilesets/catacombs_mega_special.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="catacombs" tilewidth="128" tileheight="192" tilecount="160" columns="16">
+ <image source="catacombs_mega_special.png" width="2048" height="1920"/>
+</tileset>
---
+++ Tiled/tilesets/cathedral_mega_special.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="cathedral" tilewidth="128" tileheight="192" tilecount="207" columns="16">
+ <image source="cathedral_mega_special.png" width="2048" height="2496"/>
+</tileset>
