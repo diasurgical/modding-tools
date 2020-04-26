6dc5a8942e82c97d838982c7c18be77415fec6e5
Anders Jenbo <anders@jenbo.dk>
2020-04-26 11:43:17 +0200

    Add Wang and Terrain configurations to tilesets

--- Tiled/tilesets/hell_mega.tsx
+++ Tiled/tilesets/hell_mega.tsx
@@ -2,4 +2,22 @@
 <tileset version="1.2" tiledversion="1.3.4" name="hell" tilewidth="128" tileheight="288" tilecount="144" columns="16">
  <grid orientation="isometric" width="128" height="288"/>
  <image source="hell_mega.png" width="2048" height="2592"/>
+ <terraintypes>
+  <terrain name="floor" tile="5"/>
+  <terrain name="blood" tile="29"/>
+ </terraintypes>
+ <tile id="0" terrain="1,0,1,0"/>
+ <tile id="1" terrain="1,1,0,0"/>
+ <tile id="5" terrain="0,0,0,0"/>
+ <tile id="8" terrain="1,1,1,0"/>
+ <tile id="11" terrain="1,0,0,0"/>
+ <tile id="14" terrain="0,1,0,0"/>
+ <tile id="15" terrain="0,0,1,0"/>
+ <tile id="17" terrain="0,1,0,1"/>
+ <tile id="18" terrain="0,0,1,1"/>
+ <tile id="19" terrain="1,1,1,1"/>
+ <tile id="20" terrain="0,0,0,1"/>
+ <tile id="23" terrain="0,1,1,1"/>
+ <tile id="26" terrain="1,0,1,1"/>
+ <tile id="27" terrain="1,1,0,1"/>
 </tileset>
